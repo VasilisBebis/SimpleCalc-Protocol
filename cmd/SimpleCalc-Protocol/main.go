47b1196a87966942e913c76e0805ff190daf02ab
VasilisBebis <e22113@unipi.gr>
2025-03-07 22:16:29 +0200

    File Structure Tidying

--- cmd/SimpleCalc-Protocol/main.go
+++ cmd/SimpleCalc-Protocol/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/VasilisBebis/SimpleCalc-Protocol/internal/client"
 	"github.com/VasilisBebis/SimpleCalc-Protocol/internal/server"
 )
 
 func main() {
 	fmt.Println("Hello World")
+	client.HelloClient()
+	server.HelloServer()
 }
